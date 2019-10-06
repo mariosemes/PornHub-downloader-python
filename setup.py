884f693ae5957f5b035015eb5d89ffe85a3c2a4f
Mario Semeš <mario.semes@gmail.com>
2019-10-06 09:25:18 +0200

    Changes

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='phdler',                      # This is the name of your PyPI-package.
+    version='0.1',                      # Update the version number for new releases
+    scripts=['phdler']                  # The name of your scipt, and also the command you'll be using for calling it
+)
