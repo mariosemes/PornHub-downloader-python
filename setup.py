2cb25dd470c62c01b3c93c9e012c29a680469cab
Mario Semeš <mario.semes@gmail.com>
2019-10-06 10:25:56 +0200

    Fixes

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup
 
 setup(
-    name='phdler',                      # This is the name of your PyPI-package.
-    version='0.1',                      # Update the version number for new releases
-    scripts=['phdler']                  # The name of your scipt, and also the command you'll be using for calling it
-)
+    name='phdler',    
+    version='0.1',                          
+    scripts=['phdler'],
+    author="Mario Semeš",
+    author_email="mario.semes@gmail.com",
+    description="Feel free to join my discord server: https://discord.gg/JV2NxhH"
+)  
